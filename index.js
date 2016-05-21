375173ec12698667295cd8459ea1af7f0ab04919
Joseph <joseph@joseph.srijan.in>
2016-05-21 13:47:56 +0530

    testing

--- index.js
+++ index.js
@@ -5,18 +5,11 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+process.env.PWD = process.cwd();
 
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(process.env.PWD, 'public'));
 
-// set the home page route
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index');
-});
+app.use(express.static(path.join(process.env.PWD, 'public')));
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
