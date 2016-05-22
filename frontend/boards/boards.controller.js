af4d96914e0bdbe5b085bd881db50433cca6e3a0
joseph dias <josephdias92@gmail.com>
2016-05-23 00:21:09 +0530

    dynamic sticky added to view.html

--- frontend/boards/boards.controller.js
+++ frontend/boards/boards.controller.js
@@ -1,14 +1,102 @@
 (function(module) {
-	module.controller('boardController',function(){
-		var vm=this;
-		vm.board={};
-		vm.boardtypes=[
-			{name:'Retrospective',id:1,section:['What went well','what can be improved','Action Items']},
-			{name:'Star Fish Retrospective',id:2,section:['Keep Doing','Less Of','More Of','Stop Doing','Start Doing']},
-			{name:'Six thinking hats',id:3,section:['Managing-Blue','Information-White','Emotions-Red','Discernment-Black','Optimistic response-Yellow','Creativity-Green']},
-			{name:'Todos',id:4,section:['Todos']},
-			{name:'Pros and Cons',id:5,section:['Pros','Cons']}
+	module.controller('boardController', function() {
+		var vm = this;
+		vm.board = {};
+		vm.boardtypes = [
+		{
+			name: 'Retrospective',
+			id: 1,
+			section: [{
+				name: 'What went well',
+				data: []
+			}, {
+				name: 'what can be improved',
+				data: []
+			}, {
+				name: 'Action Items',
+				data: []
+			}]
+		}, 
+		{
+			name: 'Star Fish Retrospective',
+			id: 2,
+			section: [{
+				name: 'Keep Doing',
+				data: []
+			}, {
+				name: 'Less Of',
+				data: []
+			}, {
+				name: 'More Of',
+				data: []
+			}, {
+				name: 'Stop Doing',
+				data: []
+			}, {
+				name: 'Start Doing',
+				data: []
+			}]
+		}, 
+		{
+			name: 'Six thinking hats',
+			id: 3,
+			section: [
+				{
+					name: 'Managing-Blue',
+					data: []
+				}, 
+				{
+					name: 'Information-White',
+					data: []
+				},
+				{
+					name: 'Emotions-Red',
+					data: []
+				}, 
+				{
+					name: 'Discernment-Black',
+					data: []
+				}, 
+				{
+					name: 'Optimistic response-Yellow',
+					data: []
+				}, 
+				{
+					name: 'Creativity-Green',
+					data: []
+				}
+			]
+		}, 
+		{
+			name: 'Todos',
+			id: 4,
+			section: [
+				{
+					name: 'Todos',
+					data: []
+				}
+			]
+		}, 
+		{
+			name: 'Pros and Cons',
+			id: 5,
+			section: [
+				{
+					name: 'Pros',
+					data: []
+				}, 
+				{
+					name: 'Cons',
+					data: []
+				}
+			]
+		}
 		];
-		vm.board.type=vm.boardtypes[1];
+
+		vm.board.type = vm.boardtypes[1];
+
+		vm.addSticky=function(arrRef){
+			arrRef.push({});
+		}
 	})
 })(angular.module('app.boards'));
