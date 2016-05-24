a6c6c380ee914bb8441bb4d655af4be8416883a4
Mohsin Muzawar <mohsinmuzawar@gmail.com>
2016-05-24 22:13:17 +0530

    css changes to login page

--- app/boards/boards.controller.js
+++ app/boards/boards.controller.js
@@ -95,7 +95,7 @@
     vm.board.type = vm.boardtypes[1];
 
     vm.addSticky = function(arrRef) {
-      arrRef.push({});
+      arrRef.push({text:"Enter Your Text Here"});
     };
   });
 })(angular.module('app.boards'));
