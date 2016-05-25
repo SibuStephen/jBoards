b160f2af939d3caca070875226a7458921336698
joseph dias <josephdias92@gmail.com>
2016-05-25 20:53:52 +0530

    renamed login to authentication

--- app/app.js
+++ app/app.js
@@ -2,6 +2,6 @@
   angular.module('app', [
     'app.core',
     'app.boards',
-    'app.login'
+    'app.auth'
   ]);
 })(angular);
--- app/login/authentication.module.js
+++ app/login/authentication.module.js
@@ -1,3 +1,3 @@
 (function(angular) {
-  angular.module('app.login', [])
+  angular.module('app.auth', []);
 })(angular);
